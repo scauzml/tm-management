13dddcf3fd7056c3a7eca14efdac474bacae4ab9
scauzml <13828607279@163.com>
2018-12-28 09:26:22 +0800

    测试提交

---
+++ tm-managment/src/main/java/com/springboot/mybatisplus/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.springboot.mybatisplus.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zhangmingliang
+ * @date 2018-12-28 09:25
+ */
+@Controller
+public class IndexController {
+}
