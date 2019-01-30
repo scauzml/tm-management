2ab2c684913437d287445dc2e18bd4d4a7fe60db
scauzml <13828607279@163.com>
2019-01-30 18:12:13 +0800

    在公司提交的

--- tm-managment/src/main/java/com/springboot/mybatisplus/dao/UserMapper.java
+++ tm-managment/src/main/java/com/springboot/mybatisplus/dao/UserMapper.java
@@ -1,17 +1,60 @@
 package com.springboot.mybatisplus.dao;
 
 import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.springboot.mybatisplus.entity.User;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.springboot.mybatisplus.entity.TbClass;
+import com.springboot.mybatisplus.entity.TbUser;
+import com.springboot.mybatisplus.vo.UserInformationVo;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 /**
- * @author{张明亮} 2018/12/23 21:48
+ * <p>
+ * 用户 Mapper 接口
+ * </p>
+ *
+ * @author ZML
+ * @since 2019-01-28
  */
-public interface UserMapper extends BaseMapper<User> {
-    User findByName(String name);
+@Mapper
+public interface UserMapper extends BaseMapper<TbUser> {
 
-    List<User> customSql(User user);
-
-
+    /**
+     * @author zml
+     *
+     * @param null
+     * @return
+     * 返回学生列表
+     *
+     */
+    List<TbUser> getListStudent(Page<TbUser> page, Map<String, Object> params);
+   /**
+    * @author zml
+    *
+    * @param null
+    * @return
+    * 返回教师或者其他用户
+    *
+    */
+    List<TbUser> getListTeacher(Page<TbUser> page, Map<String, Object> params);
+   /**
+    * @author zml
+    *
+    * @param null
+    * @return
+    *
+    * 返回用户个人信息
+    */
+    UserInformationVo getUserInformation(String id);
+    /**
+     * @author zml
+     *
+     * @param null
+     * @return
+     * 找回密码
+     *
+     */
+    TbUser isExitEmail(String email);
 }
